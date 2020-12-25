e4a705f983c222abc8c1518467844c13014fd28e
Ian Magallan <ian.magallan@alayacare.com>
2020-12-25 19:54:56 +0100

    Initial Commit

---
+++ OfflineServer/OfflineServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineServerApp.swift
+//  OfflineServer
+//
+//  Created by Ian Magallan on 25/12/20.
+//
+
+import SwiftUI
+
+@main
+struct OfflineServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
